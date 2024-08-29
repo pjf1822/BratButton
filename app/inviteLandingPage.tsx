ce7ff4fe5c455f49d75d989e48cddfb2d4cc5c7b
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2024-08-29 16:38:36 -0500

    i think were gucci

--- app/inviteLandingPage.tsx
+++ app/inviteLandingPage.tsx
@@ -1,14 +1,24 @@
 import { View, Text, TouchableOpacity   } from 'react-native'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { router, useLocalSearchParams } from 'expo-router';
 import { handleJoinGroup, showToast } from '@/utils';
 import { useGroupStore } from '@/zustandStore';
 import { myColors } from '@/theme';
 import { logDebugInfo } from '@/api';
+import NetInfo from '@react-native-community/netinfo';
+import Modal from 'react-native-modal';
+
+
 
 
 const inviteLandingPage = () => {
   const { groupInviteId, invitedBool, groupInviteName } = useLocalSearchParams<{ groupInviteId?: string, invitedBool: string,groupInviteName:string }>();
+const [connectedToInternet ,setConnectedToInternet] = useState(true)
+  const checkConnectivity = async () => {
+    const state = await NetInfo.fetch();
+    setConnectedToInternet(state?.isConnected)
+    return
+  };
 
   const { groupsOfUser,inviteParams ,setInviteParams,setInvited,invited} = useGroupStore((state) => ({
     groupsOfUser: state.groupsOfUser,
@@ -17,6 +27,7 @@ const inviteLandingPage = () => {
     setInvited:state.setInvited,
     invited:state.invited
   })); 
+
   
   const trySomeDebug = async () => {
     try {
@@ -28,6 +39,7 @@ const inviteLandingPage = () => {
   const firstRender = useRef(true);
 
   useEffect(() => {
+    checkConnectivity()
     trySomeDebug()
     if (groupInviteId || invitedBool || groupInviteName) {
       setInviteParams(groupInviteId, invitedBool, groupInviteName);
@@ -46,6 +58,11 @@ const inviteLandingPage = () => {
       }
     }
   }, [groupInviteId, groupsOfUser]);
+
+
+  const handleModalClose = () => {
+    setConnectedToInternet(true)
+  };
 
 
   return (
@@ -88,6 +105,14 @@ const inviteLandingPage = () => {
           </Text>
         </TouchableOpacity>
       </View>
+      <Modal isVisible={!connectedToInternet} onBackdropPress={handleModalClose}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: myColors.three, padding: 20, borderRadius: 10 }}>
+          <Text style={{ fontSize: 18, marginBottom: 40,fontFamily:"KalMedium", }}>You are not connected to the internet. Please check connection and rescan the QR code</Text>
+          {/* <TouchableOpacity onPress={handleModalClose} style={{ backgroundColor: myColors.four, padding: 10, borderRadius: 5, }}>
+            <Text style={{ color: myColors.one,fontFamily:"KalMedium", fontSize: 20 }}>Close</Text>
+          </TouchableOpacity> */}
+        </View>
+      </Modal>
    
   </>
   )
